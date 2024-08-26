802da860e97924ad957439bbeaa43c1086d8a388
devjmv <javier161890@gmail.com>
2024-08-26 23:38:34 +0200

    update NavBar component add modal for login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 import IconLogo from './icons/IconLogo.vue'
 import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
@@ -12,12 +12,9 @@ const router = useRouter()
 
 const store = useAuthStore()
 
-//localStorage.clear();
-
 async function login() {
     const response = await store.login(username.value, password.value)
-    await console.log(response);
-    
+
     if (response.message == 'Logged') {
 
         store.user.isAuthenticated = true
@@ -35,38 +32,32 @@ async function login() {
 
 </script>
 <template>
-    <div class="fixed inset-0 z-40 min-h-full overflow-y-auto overflow-x-hidden transition flex items-center">
-        <div aria-hidden="true" class="fixed inset-0 w-full h-full bg-black/50 cursor-pointer">
-            <div class="flex items-center justify-center h-screen px-6">
-                <div class="w-full max-w-sm p-6 bg-white rounded-md shadow-md">
-                    <div class="flex items-center justify-center">
-                        <IconLogo/>
-                        <span class="text-2xl font-semibold text-gray-700">Adventure Time</span>
-                    </div>
+    <div class="w-full max-w-sm p-8 px-12 bg-white rounded-md shadow-md">
+        <div class="flex items-center justify-center">
+            <IconLogo />
+            <span class="text-2xl font-semibold text-gray-700">Adventure Time</span>
+        </div>
 
-                    <form class="mt-4" @submit.prevent="login">
-                        <label class="block">
-                            <input v-model="username" type="text" placeholder="User"
-                                class="block w-full mt-4 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500">
-                        </label>
+        <form class="mt-4" @submit.prevent="login">
+            <label class="block">
+                <input v-model="username" type="text" placeholder="User"
+                    class="block w-full mt-4 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500">
+            </label>
 
-                        <label class="block mt-3">
-                            <input v-model="password" type="password" placeholder="Password"
-                                class="block w-full mt-4 border-tiffany rounded-md focus:border-tiffany focus:ring focus:ring-opacity-40 focus:ring-tiffany">
-                        </label>
+            <label class="block mt-3">
+                <input v-model="password" type="password" placeholder="Password"
+                    class="block w-full mt-4 border-tiffany rounded-md focus:border-tiffany focus:ring focus:ring-opacity-40 focus:ring-tiffany">
+            </label>
 
-                        <div class="mt-6">
-                            <button type="submit"
-                                class="w-full px-4 py-2 text-sm text-center text-white bg-verdigris rounded-md focus:outline-none hover:bg-tiffany">
-                                Sign In
-                            </button>
-                        </div>
-                    </form>
-                    <div class="flex items-center justify-center mt-4">
-                        <a class="block text-sm text-verdigris fontme hover:underline" href="#">You are not member? Sing Up</a>
-                    </div>
-                </div>
+            <div class="mt-6">
+                <button type="submit"
+                    class="w-full px-4 py-2 text-sm text-center text-white bg-verdigris rounded-md focus:outline-none hover:bg-tiffany">
+                    Sign In
+                </button>
             </div>
+        </form>
+        <div class="flex items-center justify-center mt-4">
+            <a class="block text-sm text-verdigris fontme hover:underline" href="#">You are not member? Sing Up</a>
         </div>
     </div>
 </template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,16 @@
 <script setup>
-import { ref } from 'vue'
-import IconLogo from './icons/IconLogo.vue';
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { RouterLink } from 'vue-router'
 import { useAuthStore } from '../stores/auth';
+import { ref } from 'vue'
+import IconLogo from './icons/IconLogo.vue';
 import Login from './Login.vue';
 
 const store = useAuthStore()
 const mobileMenuOpen = ref(false)
-const login = ref(false)
+//const login = ref(false)
+const open = ref(false)
 </script>
-
 <template>
     <header class="bg-white">
         <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
@@ -40,12 +41,46 @@ const login = ref(false)
                 </RouterLink>
             </div>
             <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                <RouterLink v-if="store.user.isAuthenticated" to="/logout"
+                <a href="#" v-if="!store.user.isAuthenticated" @click="open = true"
                     class="text-sm font-semibold leading-6 text-gray-900">
-                    Logout <span aria-hidden="true">&rarr;</span></RouterLink>
-                <RouterLink v-if="!store.user.isAuthenticated" to="/logout" @click="login = true"
-                    class="text-sm font-semibold leading-6 text-gray-900">
-                    Log in</RouterLink>
+                    Log in <span aria-hidden="true">&rarr;</span>
+                </a>
+                <!-- Profile dropdown -->
+                <Menu v-if="store.user.isAuthenticated" as="div" class="relative ml-3">
+                    <div>
+                        <MenuButton
+                            class="relative flex rounded-full bg-verdigris text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-verdigris">
+                            <span class="absolute -inset-1.5" />
+                            <span class="sr-only">Open user menu</span>
+                            <img class="h-8 w-8 rounded-full"
+                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                alt="" />
+                        </MenuButton>
+                    </div>
+                    <transition enter-active-class="transition ease-out duration-100"
+                        enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
+                        leave-active-class="transition ease-in duration-75"
+                        leave-from-class="transform opacity-100 scale-100"
+                        leave-to-class="transform opacity-0 scale-95">
+                        <MenuItems
+                            class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <MenuItem v-slot="{ active }">
+                            <a href="#"
+                                :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your
+                                Profile</a>
+                            </MenuItem>
+                            <MenuItem v-slot="{ active }">
+                            <a href="#"
+                                :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Settings</a>
+                            </MenuItem>
+                            <MenuItem v-slot="{ active }">
+                            <a href="/logout"
+                                :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign
+                                out</a>
+                            </MenuItem>
+                        </MenuItems>
+                    </transition>
+                </Menu>
             </div>
         </nav>
         <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
@@ -65,30 +100,54 @@ const login = ref(false)
                 <div class="mt-6 flow-root">
                     <div class="-my-6 divide-y divide-gray-500/10">
                         <div class="space-y-2 py-6">
-                            <RouterLink v-if="store.user.isAuthenticated" to="/home"
+                            <RouterLink v-if="store.user.isAuthenticated" to="/home" @click="mobileMenuOpen = false"
                                 class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                                 Home</RouterLink>
-                            <RouterLink v-if="store.user.isAuthenticated" to="/event"
+                            <RouterLink v-if="store.user.isAuthenticated" to="/event" @click="mobileMenuOpen = false"
                                 class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                                 Event</RouterLink>
                             <RouterLink v-if="store.user.isAuthenticated && store.user.role == 'ROLE_ADMIN'"
-                                to="/dashboard"
+                                @click="mobileMenuOpen = false" to="/dashboard"
                                 class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                                dashboard
+                                Dashboard
                             </RouterLink>
                         </div>
                         <div class="py-6">
-                            <RouterLink v-if="store.user.isAuthenticated" to="/logout"
+                            <RouterLink v-if="store.user.isAuthenticated" to="/logout" @click="mobileMenuOpen = false"
                                 class="text-sm font-semibold leading-6 text-gray-900">
                                 Logout <span aria-hidden="true">&rarr;</span></RouterLink>
-                            <RouterLink v-if="!store.user.isAuthenticated" to="/logout" @click="login = true"
+                            <RouterLink v-if="!store.user.isAuthenticated" to="/logout"
+                                @click="open = true, mobileMenuOpen = false"
                                 class="text-sm font-semibold leading-6 text-gray-900">
-                                Log in</RouterLink>
+                                Log in
+                            </RouterLink>
                         </div>
                     </div>
                 </div>
             </div>
         </Dialog>
     </header>
-    <Login v-if="!store.user.isAuthenticated && login == true" />
+    <TransitionRoot v-if="!store.user.isAuthenticated" as="template" :show="open">
+        <Dialog class="relative z-10" @close="open = false">
+            <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
+                leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+                <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </TransitionChild>
+
+            <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center">
+                    <TransitionChild as="template" enter="ease-out duration-300"
+                        enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
+                        leave-from="opacity-100 translate-y-0 sm:scale-100"
+                        leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+                        <DialogPanel
+                            class="relative transform overflow-hidden rounded-lg bg-white shadow-xl transition-all ">
+                            <Login />
+                        </DialogPanel>
+                    </TransitionChild>
+                </div>
+            </div>
+        </Dialog>
+    </TransitionRoot>
 </template>
