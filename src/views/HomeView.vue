c8ac7a055b0df4878467005bb2d838d369ddb3cb
tedsalvador <tedpaul@hotmail.com>
2024-08-30 13:15:45 +0200

    carrusel con taillwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup>
 import Carrusel from '@/components/Carrusel.vue';
-import Carrusel2 from '@/components/Carrusel2.vue';
 </script>
 
 <template>
   <h1>Home</h1>
-  <Carrusel />
-  <Carrusel2 />
+  <Carrusel />  
 </template>
 
 <style>
