3ba503c728a88cb608e1adbf8248e315e72190ff
devjmv <javier161890@gmail.com>
2024-09-05 01:36:25 +0200

    add to .env the vaariable size to control the amount of pagemax contain the paginate in home

--- src/stores/homeEventStore.js
+++ src/stores/homeEventStore.js
@@ -3,6 +3,8 @@ import { defineStore } from "pinia";
 
 
 const BASE_URL = import.meta.env.VITE_API_ENDPOINT;
+const PAGE_SIZE = Number(import.meta.env.VITE_PAGE_SIZE, 10);
+
 
 export const useHomeEventStore = defineStore('homeEvent', {
     state: () => ({
@@ -10,12 +12,12 @@ export const useHomeEventStore = defineStore('homeEvent', {
         isLoading: false,
         error: null,
         currentPage: 0,
-        pageSize: 5,
+        pageSize: PAGE_SIZE,
         totalPages: 0,
     }),
 
     actions: {
-        async fetchEvents(page = 0, size = 5) {
+        async fetchEvents(page = 0, size = PAGE_SIZE) {
             this.isLoading = true;
             this.error = null;
             try {
