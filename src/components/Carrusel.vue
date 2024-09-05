92a6803b111b44aca7d258a090f34e71da5683a5
devjmv <javier161890@gmail.com>
2024-09-05 12:33:23 +0200

    updated carrosel

--- src/components/Carrusel.vue
+++ src/components/Carrusel.vue
@@ -1,24 +1,39 @@
 <template>
-  <div class="relative w-full h-[400px] overflow-hidden flex justify-center items-center">
-    <div class="relative w-full h-full flex justify-center items-center">
-      <button @click="prevSlide" class="absolute top-1/2 -translate-y-1/2 bg-black bg-opacity-50 text-white px-4 py-2 cursor-pointer z-10 left-64">
-        Prev
-      </button>
-      <div
-        v-for="(image, index) in images"
-        :key="image.id"
-        class="absolute inset-0 flex justify-center items-center transition-opacity duration-500"
-        :class="{ 'opacity-100': index === currentIndex, 'opacity-0': index !== currentIndex }"
-      >
-        <img :src="image.imageUrl" :alt="image.title" class="max-h-full max-w-full object-contain rounded-[20px]" />
-        <div class="absolute bottom-5 left-1/2 transform -translate-x-1/2 text-center text-white bg-black bg-opacity-50 p-2 rounded-[10px]">
-          <h3>{{ image.title }}</h3>
-          <p>{{ image.description }}</p>
+  <div class="flex justify-center align-middle">
+    <div class="relative w-full h-[400px] overflow-hidden flex justify-center items-center">
+      <div class="relative w-full h-full flex justify-center items-center">
+        <button type="button" @click="prevSlide"
+          class="flex absolute top-0 left-14 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
+          data-carousel-prev>
+          <span
+            class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-primary group-hover:bg-secondary group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor"
+              viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+            </svg>
+            <span class="hidden">Previous</span>
+          </span>
+        </button>
+        <div v-for="(image, index) in images" :key="image.id"
+          class="absolute inset-0 items-center transition-opacity duration-500"
+          :class="{ 'opacity-100': index == currentIndex, 'opacity-0': index != currentIndex }">
+          <Card :id="image.id" :title="image.title" :eventDateTime="image.eventDateTime"
+            :maxParticipants="image.maxParticipants" :participantsCount="image.participantsCount"
+            :description="image.description" :imageUrl="image.imageUrl" :registered="image.registered" />
         </div>
+        <button type="button" @click="nextSlide"
+          class="flex absolute top-0 right-14 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
+          data-carousel-next>
+          <span
+            class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-primary group-hover:bg-secondary group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-5 h-5 text-dark sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor"
+              viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+            </svg>
+            <span class="hidden">Next</span>
+          </span>
+        </button>
       </div>
-      <button @click="nextSlide" class="absolute top-1/2 -translate-y-1/2 bg-black bg-opacity-50 text-white px-4 py-2 cursor-pointer z-10 right-64">
-        Next
-      </button>
     </div>
   </div>
 </template>
@@ -26,13 +41,21 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
+import Card from './Card.vue';
+import {
+  Carousel,
+  initTWE,
+} from "tw-elements";
+
+initTWE({ Carousel });
 
 const images = ref([]);
 const currentIndex = ref(0);
+const baseUrl = import.meta.env.VITE_API_ENDPOINT
 
 const fetchImages = async () => {
   try {
-    const response = await axios.get('http://localhost:8080/api/v1/event/featured');
+    const response = await axios.get(baseUrl + '/event/featured');
     images.value = response.data;
   } catch (error) {
     console.error('Error fetching images:', error);
@@ -50,6 +73,6 @@ const prevSlide = () => {
 
 onMounted(async () => {
   await fetchImages();
-  setInterval(nextSlide, 3000);
+  setInterval(nextSlide, 5000);
 });
 </script>
